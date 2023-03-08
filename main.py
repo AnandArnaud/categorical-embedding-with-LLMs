65a7a43198f914008337ba83a0292664d6c7132c
Anand-Arnaud Pajaniradjane <apajanir@drago3.saclay.inria.fr>
2023-03-08 17:49:20 +0100

    results on 5 datasets

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from dirty_cat import datasets
+import pandas as pd
 
 from functions.utils import save_scores_to_csv
-from functions.data_loader import load_datasets_from_dirty_cat
+from functions.data_loader import load_datasets, fetch_colleges
 from functions.SuperVectorizer_baseline import run_baseline_model
 from functions.bert_encoder import run_model_using_bert_embeddings
 
 
-all_datasets = load_datasets_from_dirty_cat()
+all_datasets = load_datasets()
+# small_datasets = {"colleges" : fetch_colleges()}
 
 baseline_results_df = run_baseline_model(all_datasets)
 save_scores_to_csv(baseline_results_df, "baseline_results.csv")
 
-
-results_using_bert_embeddings_df = run_model_using_bert_embeddings(all_datasets, 
+results_using_bert_embeddings_df1 = run_model_using_bert_embeddings(all_datasets, 
                                                                         sentence_strategy="only_value", 
                                                                         embedding_strategy="last_four_hidden_state_concatenate"
                                                                     )
-save_scores_to_csv(results_using_bert_embeddings_df, "bert_only_value_four_hidden_state_results.csv")
 
-
-results_using_bert_embeddings_df = run_model_using_bert_embeddings(all_datasets, 
+results_using_bert_embeddings_df2 = run_model_using_bert_embeddings(all_datasets, 
                                                                         sentence_strategy="value_and_column", 
                                                                         embedding_strategy="last_four_hidden_state_concatenate"
                                                                     )
-save_scores_to_csv(results_using_bert_embeddings_df, "bert_value_col_foour_hidden_state_results.csv")
 
-
-results_using_bert_embeddings_df = run_model_using_bert_embeddings(all_datasets, 
+results_using_bert_embeddings_df3 = run_model_using_bert_embeddings(all_datasets, 
                                                                         sentence_strategy="value_and_column", 
                                                                         embedding_strategy="last_hidden_state"
                                                                     )
-save_scores_to_csv(results_using_bert_embeddings_df, "bert_value_col_last_hidden_state_results.csv")
+
+all_results_df = pd.concat([baseline_results_df,
+                            results_using_bert_embeddings_df1,
+                            results_using_bert_embeddings_df2,
+                            results_using_bert_embeddings_df3
+                            ],
+                            axis = 0)
+
+all_results_df = all_results_df.sort_values(["dataset_name", "strategy"])
+
+save_scores_to_csv(all_results_df, "results.csv")
