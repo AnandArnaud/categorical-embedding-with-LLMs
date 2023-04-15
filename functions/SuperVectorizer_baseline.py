26ba757a04ff739c898f4faa4cef9d397a85fc86
Anand-Arnaud Pajaniradjane <apajaniradjane@gmail.com>
2023-04-15 22:46:35 +0200

    argument modification

--- functions/SuperVectorizer_baseline.py
+++ functions/SuperVectorizer_baseline.py
@@ -8,34 +8,29 @@ from sklearn.model_selection import cross_val_score
 from functions.utils import get_pipeline, get_scoring
 
 
-def run_baseline_model(all_datasets):
-    results = []
+def run_baseline_model(dataset):
 
-    for dataset_name, dataset in all_datasets.items():
-        start = time.time()
-        X = dataset.X
-        y = dataset.y
+    start = time.time()
+    X = dataset.X
+    y = dataset.y
 
-        target_type = y.dtype.name
-        pipeline = get_pipeline(target_type)
-        scoring = get_scoring(target_type)
+    target_type = y.dtype.name
+    pipeline = get_pipeline(target_type)
+    scoring = get_scoring(target_type)
 
-        print(dataset_name)
+    print(dataset_name)
 
-        scores = cross_val_score(pipeline, X, y, scoring=scoring, n_jobs=-1)
-        print(np.mean(scores))
-        end = time.time()
-        time_delta = datetime.timedelta(seconds = end-start)
+    scores = cross_val_score(pipeline, X, y, scoring=scoring, n_jobs=-1)
+    print(np.mean(scores))
+    end = time.time()
+    time_delta = datetime.timedelta(seconds = end-start)
 
-        result = pd.DataFrame({
-            'dataset_name': [dataset_name],
-            "strategy" : ["SuperVectorizer"],
-            'mean_score': [np.mean(scores)],
-            'std_score': [np.std(scores)],
-            "compute_time" : [time_delta]
-        })
-        results.append(result)
+    result = pd.DataFrame({
+        'dataset_name': [dataset_name],
+        "strategy" : ["SuperVectorizer"],
+        'mean_score': [np.mean(scores)],
+        'std_score': [np.std(scores)],
+        "compute_time" : [time_delta]
+    })
 
-    results_df = pd.concat(results)
-
-    return results_df
+    return result 
